b8cbae4104e60a6ddad9d78b9576f3615ec92339
msleigh <msleigh@users.noreply.github.com>
2023-12-06 23:52:08 +0000

    Multiple plot types

--- mccc/monte_carlo.py
+++ mccc/monte_carlo.py
@@ -2,11 +2,11 @@
 import sys
 
 import click
+import numpy as np
 import pandas as pd
-from numpy import mean
-from numpy import std
 
 from mccc.geometry import update_neutron_position
+from mccc.plotting import plot_generations
 from mccc.plotting import plot_particle_convergence
 from mccc.plotting import plot_starting_positions
 from mccc.sampling import sample_direction_cosine
@@ -106,9 +106,12 @@ def run(generations, particles, plot=True):
         sample_position(slab_thickness_cm) for _ in range(num_particles_in_generation)
     ]
 
+    k1 = []
+    k2 = []
+
     for gen in range(num_generations):
-        if gen >= 2 and plot:
-            plot_starting_positions(start_positions)
+        if gen >= 0 and plot:
+            plot_starting_positions(num_generations, gen, start_positions)
 
         # Reset all the tallies to zero for this generation
         tallies = initialise_tallies()
@@ -132,12 +135,12 @@ def run(generations, particles, plot=True):
             sys.exit("Zero collisions")
 
         # Estimate k_eff
-        k1 = (
+        k1.append(
             nu
             * tallies["fission"]
             / (tallies["capture"] + tallies["leakage"] + tallies["fission"])
         )
-        k2 = len(next_start_positions) / num_particles_in_generation
+        k2.append(len(next_start_positions) / num_particles_in_generation)
         c = tallies["secondary"] / tallies["collision"]
 
         verbose = False
@@ -176,7 +179,7 @@ def run(generations, particles, plot=True):
         start_positions = next_start_positions
 
     if plot:
-        plot_starting_positions()
+        plot_starting_positions(num_generations, gen)
 
     return k1, k2
 
@@ -186,8 +189,38 @@ def trial(generations, particles):
     Run a trial; a set of n independent but identical runs, averaged over.
     """
     n = 10
-    k1, k2 = zip(*[run(generations, particles, plot=False) for _ in range(n)])
-    return mean(k1), std(k1), mean(k2), std(k2)
+    k1, k2 = zip(*[run(generations, particles) for _ in range(n)])
+    k1m = np.mean(np.array(k1), axis=0)
+    k1s = np.std(np.array(k1), axis=0)
+    k2m = np.mean(np.array(k2), axis=0)
+    k2s = np.std(np.array(k2), axis=0)
+    return (k1m, k1s, k2m, k2s)
+
+
+def study_convergence(generations, particles_list):
+    data = []
+    for particles in particles_list:
+        data.append([series[-1] for series in trial(generations, particles)])
+    df = pd.DataFrame(
+        data, index=particles_list, columns=["k1", "k1_std", "k2", "k2_std"]
+    )
+    plot_particle_convergence(df)
+
+
+def study_generations(generations, particles):
+    data = trial(generations, particles)
+    print(data)
+    df = pd.DataFrame(
+        np.transpose(data),
+        index=range(generations),
+        columns=["k1", "k1_std", "k2", "k2_std"],
+    )
+    print(df)
+    plot_generations(df)
+
+
+def study_fission_rate(generations, particles):
+    run(generations, particles, plot=True)
 
 
 @click.command()
@@ -200,11 +233,19 @@ def trial(generations, particles):
     multiple=True,
     help="Number of particles.",
 )
-def main(generations, particles_list):
-    data = []
-    for particles in particles_list:
-        data.append(trial(generations, particles))
-    df = pd.DataFrame(
-        data, index=particles_list, columns=["k1", "k1_std", "k2", "k2_std"]
-    )
-    plot_particle_convergence(df)
+@click.option(
+    "plot_type", "-t", "--type", default="convergence", help="Type of plot to create."
+)
+def main(generations, particles_list, plot_type):
+    if plot_type == "convergence":
+        if len(particles_list) < 2:
+            sys.exit("Not enough -p values")
+        study_convergence(generations, particles_list)
+    elif plot_type == "generations":
+        if len(particles_list) != 1:
+            sys.exit("Too many -p values")
+        study_generations(generations, particles_list[0])
+    elif plot_type == "fission_rate":
+        if len(particles_list) != 1:
+            sys.exit("Too many -p values")
+        study_fission_rate(generations, particles_list[0])
--- mccc/plotting.py
+++ mccc/plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_starting_positions(starting_positions=None):
+def plot_starting_positions(num_generations, gen, starting_positions=None):
     """
     Function to create a histogram and plot starting positions.
 
@@ -11,7 +11,7 @@ def plot_starting_positions(starting_positions=None):
     - starting_positions (list): List of starting positions (x-values).
     """
     if starting_positions is None:
-        plt.show()
+        plt.savefig("fission_rate.png")
         return
 
     # Calculate the histogram data
@@ -23,13 +23,15 @@ def plot_starting_positions(starting_positions=None):
     # Normalise
     hist = hist / len(starting_positions)
 
+    colors = plt.cm.jet(np.linspace(0, 1, num_generations))
+
     # Create a line plot for frequencies
-    plt.plot(bin_centers, hist, color="blue", marker="o", linestyle="-")
+    plt.plot(bin_centers, hist, color=colors[gen], linestyle="-")
 
     # Set plot labels and title
     plt.xlabel("Starting position (cm)")
     plt.ylabel("Frequency")
-    plt.title("Histogram of Starting positions")
+    plt.title("Histogram of starting positions by generation")
     return
 
 
@@ -49,4 +51,22 @@ def plot_particle_convergence(df):
         title="Convergence of $k_{\mathrm{eff}}$ with no. of particles.",
     )
     plt.savefig("particle_convergence.png")
+    return
+
+
+def plot_generations(df):
+    """
+    Function to plot k1 and k2 as a function of generation number.
+    """
+    df.plot(
+        y=["k1", "k2"],
+        grid=True,
+        yerr=[df["k1_std"], df["k2_std"]],
+        xticks=df.index,
+        ylim=(0.97, 1.03),
+        xlabel="Generation number",
+        ylabel="$k_{\mathrm{eff}}$",
+        title="Convergence of $k_{\mathrm{eff}}$ with generation",
+    )
+    plt.savefig("generations.png")
     return
